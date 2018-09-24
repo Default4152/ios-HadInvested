2bd1dd65cb621ad457eecdc60d7ab7028f2883b9
Conner Alegre <cga0@pm.me>
2018-09-24 11:58:43 -0600

    add base view controllers for appropriate views in sb

---
+++ HadInvested/View Controllers/CalculateResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CalculateResultViewController.swift
+//  HadInvested
+//
+//  Created by Conner on 9/24/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class CalculateResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ HadInvested/View Controllers/CalculateViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CalculateViewController.swift
+//  HadInvested
+//
+//  Created by Conner on 9/24/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
