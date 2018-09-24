f1b1b3166caeae860d410ffeccc0a49fa600bbd7
Conner Alegre <cga0@pm.me>
2018-09-24 14:16:22 -0600

    stock data codable

--- HadInvested/Models/StockData.swift
+++ HadInvested/Models/StockData.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+struct StockData: Codable {
+    let timeSeriesDaily: [String: TimeSeriesDaily]
+    
+    enum CodingKeys: String, CodingKey {
+        case timeSeriesDaily = "Time Series (Daily)"
+    }
+}
+
+struct TimeSeriesDaily: Codable {
+    let open, high, low, close: String
+    let volume: String
+    
+    enum CodingKeys: String, CodingKey {
+        case open = "1. open"
+        case high = "2. high"
+        case low = "3. low"
+        case close = "4. close"
+        case volume = "5. volume"
+    }
+}
