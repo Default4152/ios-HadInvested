d3a01a06d348d2ca3a447c47be98a58b899303d6
Conner Alegre <cga0@pm.me>
2018-09-25 15:08:14 -0600

    move logic for calculation into detail view and display

--- HadInvested/View Controllers/CalculateResultViewController.swift
+++ HadInvested/View Controllers/CalculateResultViewController.swift
@@ -7,11 +7,83 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
-class CalculateResultViewController: UIViewController {
-
+class CalculateResultViewController: UIViewController, NVActivityIndicatorViewable {
+    var amount: String?
+    var symbol: String?
+    var isCrypto: Bool = false
+    var apiController = APIController()
+    let date = Calendar.current.date(byAdding: .day, value: -1, to: Date())
+    let formatter = DateFormatter()
+    var finalAmount = 0.0
+    var datePicker: UIDatePicker?
+    
+    @IBOutlet var amountHadInvestedLabel: UILabel!
+    @IBOutlet var symbolLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var finalAmountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        formatter.dateFormat = "yyyy-MM-dd"
+        
+        fetchData()
+    }
+    
+    func fetchData() {
+        guard let amountHadInvestedLabel = amountHadInvestedLabel,
+            let symbolLabel = symbolLabel,
+            let dateLabel = dateLabel,
+            let finalAmountLabel = finalAmountLabel,
+            let amount = amount,
+            let symbol = symbol,
+            let datePicker = datePicker,
+            let date = date else { return }
+        
+        let chosenDate = formatter.string(from: datePicker.date)
+        let yesterdayAsString = self.formatter.string(from: date)
+        
+        if isCrypto {
+            apiController.getCryptoData(with: symbol) { (cryptoData) in
+                guard let cryptoData = cryptoData,
+                    let currentPrice = cryptoData.cryptoDaily[yesterdayAsString]?["1a. open (USD)"],
+                    let chosenDateClosePrice = cryptoData.cryptoDaily[chosenDate]?["4a. close (USD)"],
+                    let amount = Double(amount),
+                    let closePriceAsDouble = Double(chosenDateClosePrice),
+                    let currentPriceAsDouble = Double(currentPrice) else {
+                        NVActivityIndicatorPresenter.sharedInstance.stopAnimating(nil)
+                        return
+                }
+                
+                let currencyPurchased = amount / closePriceAsDouble
+                DispatchQueue.main.async {
+                    finalAmountLabel.text = "$\(String(format: "%.2f", currentPriceAsDouble * currencyPurchased))"
+                    amountHadInvestedLabel.text = "$\(String(format: "%.2f", amount))"
+                    symbolLabel.text = symbol
+                    dateLabel.text = chosenDate
+                }
+                NVActivityIndicatorPresenter.sharedInstance.stopAnimating(nil)
+            }
+        } else {
+            apiController.getStockData(with: symbol) { (stockData) in
+                guard let stockData = stockData,
+                    let currentPrice = stockData.timeSeriesDaily[yesterdayAsString]?.close,
+                    let chosenDateClosePrice = stockData.timeSeriesDaily[chosenDate]?.close,
+                    let amount = Double(amount),
+                    let closePriceAsDouble = Double(chosenDateClosePrice),
+                    let currentPriceAsDouble = Double(currentPrice) else {
+                        NVActivityIndicatorPresenter.sharedInstance.stopAnimating(nil)
+                        return
+                }
+                let stockPurchased = amount / closePriceAsDouble
+                self.finalAmount = currentPriceAsDouble * stockPurchased
+                NVActivityIndicatorPresenter.sharedInstance.stopAnimating(nil)
+            }
+        }
     }
 
+    @IBAction func addRegret(_ sender: Any) {
+        // add regret and pop view controller
+    }
 }
--- HadInvested/View Controllers/CalculateViewController.swift
+++ HadInvested/View Controllers/CalculateViewController.swift
@@ -10,9 +10,6 @@ import UIKit
 import NVActivityIndicatorView
 
 class CalculateViewController: UIViewController, NVActivityIndicatorViewable {
-    let apiController = APIController()
-    let date = Calendar.current.date(byAdding: .day, value: -1, to: Date())
-    let formatter = DateFormatter()
     let activityData = ActivityData(size: nil,
                                     message: "Calculating...",
                                     messageFont: nil,
@@ -30,50 +27,19 @@ class CalculateViewController: UIViewController, NVActivityIndicatorViewable {
     @IBOutlet var datePicker: UIDatePicker!
     @IBOutlet var isCrypto: UISwitch!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        formatter.dateFormat = "yyyy-MM-dd"
-    }
-
     @IBAction func calculate(_ sender: Any) {
         NVActivityIndicatorPresenter.sharedInstance.startAnimating(activityData, nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let symbol = symbolTextField.text,
-            let amount = amountTextField.text,
-            let yesterday = date else { return }
-
-        let chosenDate = formatter.string(from: datePicker.date)
-        let yesterdayAsString = self.formatter.string(from: yesterday)
-
-        if isCrypto.isOn {
-            apiController.getCryptoData(with: symbol) { (cryptoData) in
-                guard let cryptoData = cryptoData,
-                    let currentPrice = cryptoData.cryptoDaily[yesterdayAsString]?["1a. open (USD)"],
-                    let chosenDateClosePrice = cryptoData.cryptoDaily[chosenDate]?["4a. close (USD)"],
-                    let amount = Double(amount),
-                    let closePriceAsDouble = Double(chosenDateClosePrice),
-                    let currentPriceAsDouble = Double(currentPrice) else {
-                        NVActivityIndicatorPresenter.sharedInstance.stopAnimating(nil)
-                        return
-                }
-
-                let currencyPurchased = amount / closePriceAsDouble
-                print(currentPriceAsDouble * currencyPurchased)
-                NVActivityIndicatorPresenter.sharedInstance.stopAnimating(nil)
-            }
-        } else {
-            apiController.getStockData(with: symbol) { (stockData) in
-                guard let stockData = stockData,
-                    let currentPrice = stockData.timeSeriesDaily[yesterdayAsString]?.close,
-                    let chosenDateClosePrice = stockData.timeSeriesDaily[chosenDate]?.close,
-                    let amount = Double(amount),
-                    let closePriceAsDouble = Double(chosenDateClosePrice),
-                    let currentPriceAsDouble = Double(currentPrice) else {
-                        NVActivityIndicatorPresenter.sharedInstance.stopAnimating(nil)
-                        return
-                }
-                let stockPurchased = amount / closePriceAsDouble
-                print(currentPriceAsDouble * stockPurchased)
-                NVActivityIndicatorPresenter.sharedInstance.stopAnimating(nil)
+            let amount = amountTextField.text else { return }
+        if segue.identifier == "ShowCalculationSegue" {
+            if let calculatedVC = segue.destination as? CalculateResultViewController {
+                calculatedVC.amount = amount
+                calculatedVC.symbol = symbol
+                calculatedVC.isCrypto = isCrypto.isOn
+                calculatedVC.datePicker = datePicker
             }
         }
     }
